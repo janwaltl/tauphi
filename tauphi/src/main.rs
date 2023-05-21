165dadfa8c1f34ec7ce39f9b4b96dcadff262985
Jan Waltl <waltl.jan@gmail.com>
2023-05-21 16:22:43 +0200

    docs: Move examples to docs, fix links

--- tauphi/src/main.rs
+++ tauphi/src/main.rs
@@ -1,25 +1,11 @@
 use perf_event::sampling;
 
-fn sync_main() {
-    let sampler = sampling::Sampler::new_cpu(0, 100).expect("Failed to start the sampling.");
-
-    for val in sampler.take(10) {
-        println!("Value: {:#?}", val);
-    }
-
-    println!("Hello, world!");
-}
-
-async fn async_main() {
+#[tokio::main]
+async fn main() {
     let sampler = sampling::Sampler::new_cpu(0, 5).expect("Failed to start the sampling.");
     let sampler = sampling::AsyncSampler::from_sync(sampler).unwrap();
     for i in 1..10 {
         let sample = sampler.get_sample().await.unwrap();
         println!("#{i} {:#?}", sample);
     }
-}
-
-#[tokio::main]
-async fn main() {
-    async_main().await;
 }
